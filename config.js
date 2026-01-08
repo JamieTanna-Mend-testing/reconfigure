137dccf40c9fe543a3f988130a951d08db5b213a
Jamie Tanna <jamie.tanna@mend.io>
2026-01-08 17:22:43 +0000

    sq

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 module.exports = {
 	allowedEnv: [
-		'SOME_ENV'
+		'GO_*',
+		'SOME_ENV',
+		'MEND_VENDIR_GITHUB_API_TOKEN'
 	],
 	secrets: {
 		SOME_ENV: 'foo',
+		MEND_VENDIR_GITHUB_API_TOKEN: 'foo',
 	}
 }
